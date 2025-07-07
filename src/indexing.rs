1b65f2696676b0fd33aa5e76be23e257143a856f
Franklin Delehelle <franklin.delehelle@odena.eu>
2025-07-07 15:36:39 +0200

    refactor: move indexing machinery in Index

--- src/indexing.rs
+++ src/indexing.rs
@@ -1,21 +1,32 @@
-use std::{collections::HashMap, ops::ControlFlow, path::Path};
-
-use async_lsp::{
-    concurrency::{Concurrency, ConcurrencyLayer},
-    lsp_types::{
-        notification::{Progress, PublishDiagnostics, ShowMessage},
-        CallHierarchyIncomingCallsParams, CallHierarchyItem, CallHierarchyOutgoingCallsParams,
-        NumberOrString, PartialResultParams, ProgressParamsValue, SymbolInformation,
-        WorkDoneProgress, WorkDoneProgressBegin, WorkDoneProgressEnd, WorkDoneProgressParams,
-        WorkDoneProgressReport,
-    },
-    panic::{CatchUnwind, CatchUnwindLayer},
-    router::Router,
-    tracing::{Tracing, TracingLayer},
-    MainLoop, ServerSocket,
+use std::{
+    collections::{HashMap, HashSet},
+    ops::ControlFlow,
+    path::Path,
+    process::Stdio,
 };
+
+use anyhow::Context;
+use async_lsp::{
+    LanguageServer, ServerSocket,
+    concurrency::ConcurrencyLayer,
+    lsp_types::{
+        CallHierarchyIncomingCall, CallHierarchyIncomingCallsParams, CallHierarchyItem,
+        CallHierarchyOutgoingCall, CallHierarchyOutgoingCallsParams, ClientCapabilities,
+        InitializeParams, InitializedParams, NumberOrString, PartialResultParams,
+        ProgressParamsValue, SymbolInformation, SymbolKind, TraceValue, Url,
+        WindowClientCapabilities, WorkDoneProgress, WorkDoneProgressBegin, WorkDoneProgressEnd,
+        WorkDoneProgressParams, WorkDoneProgressReport, WorkspaceFolder, WorkspaceSymbolParams,
+        WorkspaceSymbolResponse,
+        notification::{Progress, PublishDiagnostics, ShowMessage},
+    },
+    panic::CatchUnwindLayer,
+    router::Router,
+    tracing::TracingLayer,
+};
+use async_process::Child;
 use colored::Colorize;
 use log::info;
+use tokio::task::JoinHandle;
 use tower::ServiceBuilder;
 
 pub struct ClientState {
@@ -26,7 +37,7 @@ pub struct Stop;
 
 pub type CallHierarchyCache = HashMap<Function, (Vec<CallSite>, Vec<CallSite>)>;
 
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct CallSite(pub CallHierarchyItem);
 impl CallSite {
     pub fn pretty<P: AsRef<Path>>(&self, root: P) -> String {
@@ -107,12 +118,12 @@ impl From<&Function> for CallHierarchyItem {
     fn from(f: &Function) -> Self {
         CallHierarchyItem {
             name: f.0.name.clone(),
-            kind: f.0.kind.clone(),
+            kind: f.0.kind,
             tags: f.0.tags.clone(),
             detail: None,
             uri: f.0.location.uri.clone(),
-            range: f.0.location.range.clone(),
-            selection_range: f.0.location.range.clone(),
+            range: f.0.location.range,
+            selection_range: f.0.location.range,
             data: None,
         }
     }
@@ -146,20 +157,32 @@ impl From<&Function> for CallHierarchyOutgoingCallsParams {
 
 const RA_INDEXING_TOKENS: &[&str] = &["rustAnalyzer/Indexing", "rustAnalyzer/cachePriming"];
 
-pub fn run_server() -> (
-    (
-        MainLoop<Tracing<CatchUnwind<Concurrency<Router<ClientState>>>>>,
-        ServerSocket,
-    ),
-    oneshot::Receiver<()>,
-) {
-    let (indexed_tx, indexed_rx) = oneshot::channel();
-    (
-        async_lsp::MainLoop::new_client(|_server| {
-            let mut router = Router::new(ClientState {
-                indexed_tx: Some(indexed_tx),
-            });
-            router
+pub struct Index {
+    /// A handle to the LS interaction loop
+    mainloop_fut: JoinHandle<()>,
+    /// A socket to talk to the LS.
+    server: ServerSocket,
+    /// A handle to the LS process.
+    _child: Child,
+
+    clutter: HashSet<String>,
+    pub functions: Vec<Function>,
+    cache: CallHierarchyCache,
+}
+impl Index {
+    pub async fn new<P: AsRef<Path>>(
+        root: P,
+        clutter: HashSet<String>,
+        ra_bin: &str,
+    ) -> anyhow::Result<Self> {
+        let (indexed_tx, indexed_rx) = oneshot::channel();
+
+        let ((mainloop, mut server), indexed_rx) = (
+            async_lsp::MainLoop::new_client(|_server| {
+                let mut router = Router::new(ClientState {
+                    indexed_tx: Some(indexed_tx),
+                });
+                router
             .notification::<Progress>(|this, prog| {
                 let token = match &prog.token{
                     NumberOrString::Number(x) => x.to_string(),
@@ -192,12 +215,204 @@ pub fn run_server() -> (
             })
             .event(|_, _: Stop| ControlFlow::Break(Ok(())));
 
-            ServiceBuilder::new()
-                .layer(TracingLayer::default())
-                .layer(CatchUnwindLayer::default())
-                .layer(ConcurrencyLayer::default())
-                .service(router)
-        }),
-        indexed_rx,
-    )
+                ServiceBuilder::new()
+                    .layer(TracingLayer::default())
+                    .layer(CatchUnwindLayer::default())
+                    .layer(ConcurrencyLayer::default())
+                    .service(router)
+            }),
+            indexed_rx,
+        );
+
+        let mut _child = async_process::Command::new(ra_bin)
+            .current_dir(&root)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(std::io::stderr())
+            .kill_on_drop(true)
+            .spawn()
+            .expect("Failed run rust-analyzer");
+        let stdout = _child.stdout.take().unwrap();
+        let stdin = _child.stdin.take().unwrap();
+
+        let mainloop_fut = tokio::spawn(async move {
+            mainloop.run_buffered(stdout, stdin).await.unwrap();
+        });
+
+        // Initialize.
+        let init_ret = server
+            .initialize(InitializeParams {
+                workspace_folders: Some(vec![WorkspaceFolder {
+                    uri: Url::from_file_path(&root).unwrap(),
+                    name: "root".into(),
+                }]),
+                capabilities: ClientCapabilities {
+                    window: Some(WindowClientCapabilities {
+                        work_done_progress: Some(true),
+                        ..WindowClientCapabilities::default()
+                    }),
+                    ..ClientCapabilities::default()
+                },
+                trace: Some(TraceValue::Verbose),
+                work_done_progress_params: WorkDoneProgressParams {
+                    work_done_token: Some(NumberOrString::String("GGGGG".into())),
+                },
+                initialization_options: Some(
+                    serde_json::from_str(
+                        r#"{
+"files": {"excludeDirs": [".direnv", ".devenv"]},
+"workspace": {"symbol": {"search": {"limit": 10000, "kind": "all_symbols", "scope": "workspace"}}},
+"cargo": {"targetDir": "target/terrier"}
+}"#,
+                    )
+                    .unwrap(),
+                ),
+                ..InitializeParams::default()
+            })
+            .await
+            .unwrap();
+        info!("Initialized: {init_ret:?}");
+        server.initialized(InitializedParams {}).unwrap();
+
+        indexed_rx.await.unwrap();
+        info!("Project indexed.");
+
+        let mut functions = HashSet::new();
+        let ret = server
+            .symbol(WorkspaceSymbolParams {
+                partial_result_params: PartialResultParams {
+                    partial_result_token: None,
+                },
+                work_done_progress_params: WorkDoneProgressParams {
+                    work_done_token: None,
+                },
+                query: "".into(),
+            })
+            .await
+            .context("fetching symbols")?;
+
+        if let Some(symbols) = ret {
+            match symbols {
+                WorkspaceSymbolResponse::Flat(symbols) => {
+                    for ref s @ SymbolInformation {
+                        ref name,
+                        ref kind,
+                        ref container_name,
+                        ..
+                    } in symbols
+                    {
+                        if matches!(*kind, SymbolKind::FUNCTION) && !clutter.contains(name) {
+                            functions.insert(Function(s.clone()));
+                        }
+                        info!(
+                            "{:?} {}::{}",
+                            kind,
+                            container_name.clone().unwrap_or_default(),
+                            name
+                        );
+                    }
+                }
+                WorkspaceSymbolResponse::Nested(_workspace_symbols) => todo!(),
+            }
+        } else {
+            info!("None.");
+        }
+
+        let mut functions = functions.into_iter().collect::<Vec<_>>();
+        info!("{} functions found", functions.len());
+        functions.sort_by(|f1, f2| {
+            f1.0.location.uri.cmp(&f2.0.location.uri).then(
+                f1.0.location
+                    .range
+                    .start
+                    .line
+                    .cmp(&f2.0.location.range.start.line),
+            )
+        });
+
+        Ok(Self {
+            // Keep a handle to the LS process so that it does not die
+            _child,
+            clutter,
+            functions,
+            cache: Default::default(),
+            mainloop_fut,
+            server,
+        })
+    }
+
+    pub async fn context(&mut self, f_id: usize) -> anyhow::Result<(Vec<CallSite>, Vec<CallSite>)> {
+        let (incomings, outgoings) = {
+            if let Some(callsites) = self.cache.get(&self.functions[f_id]) {
+                callsites
+            } else {
+                let mut incomings = Vec::new();
+                let mut outgoings = Vec::new();
+
+                if let Some(xs) = self
+                    .server
+                    .incoming_calls((&self.functions[f_id]).into())
+                    .await
+                    .context("failed to fetch incomings")?
+                {
+                    let mut incomings_set = HashSet::new();
+                    for CallHierarchyIncomingCall {
+                        from: ff @ CallHierarchyItem { name, .. },
+                        ..
+                    } in xs.iter()
+                    {
+                        if !self.clutter.contains(name) {
+                            incomings_set.insert(CallSite(ff.clone()));
+                        }
+                    }
+                    incomings.extend(incomings_set.into_iter());
+                }
+
+                if let Some(xs) = self
+                    .server
+                    .outgoing_calls((&self.functions[f_id]).into())
+                    .await
+                    .context("failed to fetch outgoings")?
+                {
+                    let mut outgoings_set = HashSet::new();
+                    for CallHierarchyOutgoingCall {
+                        to: ff @ CallHierarchyItem { name, .. },
+                        ..
+                    } in xs.iter()
+                    {
+                        if !self.clutter.contains(name) {
+                            outgoings_set.insert(CallSite(ff.clone()));
+                        }
+                    }
+                    outgoings.extend(outgoings_set.into_iter());
+                }
+
+                incomings.sort_by(|f1, f2| {
+                    f1.0.uri
+                        .cmp(&f2.0.uri)
+                        .then(f1.0.range.start.line.cmp(&f2.0.range.start.line))
+                });
+                outgoings.sort_by(|f1, f2| {
+                    f1.0.uri
+                        .cmp(&f2.0.uri)
+                        .then(f1.0.range.start.line.cmp(&f2.0.range.start.line))
+                });
+
+                let f = self.functions[f_id].clone();
+                self.cache.insert(f.clone(), (incomings, outgoings));
+                self.cache.get(&f).unwrap()
+            }
+        };
+
+        Ok((incomings.to_vec(), outgoings.to_vec()))
+    }
+
+    pub async fn shutdown(mut self) -> anyhow::Result<()> {
+        self.server.shutdown(()).await.unwrap();
+        self.server.exit(()).unwrap();
+
+        self.server.emit(Stop).unwrap();
+        self.mainloop_fut.await?;
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-use std::{
-    collections::HashSet, io::stderr, path::Path, process::Stdio
-};
+use std::path::Path;
 
 use anyhow::{Context, ensure};
-use async_lsp::{
-    LanguageServer,
-    lsp_types::{
-        CallHierarchyIncomingCall, CallHierarchyItem,
-        CallHierarchyOutgoingCall, ClientCapabilities,
-        InitializeParams, InitializedParams, NumberOrString, PartialResultParams,
-        SymbolInformation, SymbolKind, TraceValue, Url,
-        WindowClientCapabilities, WorkDoneProgressParams, WorkspaceFolder, WorkspaceSymbolParams,
-        WorkspaceSymbolResponse,
-    },
-};
 use clap::Parser;
 use dialoguer::FuzzySelect;
 use fern::colors::{Color, ColoredLevelConfig};
-use indexing::{CallHierarchyCache, CallSite, Function, Stop};
-use log::{info, warn};
+use indexing::Index;
 use spinoff::{Spinner, spinners};
 use tabled::tables::IterTable;
 
@@ -40,13 +26,9 @@ struct Args {
     clutter: Vec<String>,
 }
 
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    let mut cache: CallHierarchyCache = Default::default();
-    
-    let colors = ColoredLevelConfig::new()
-        .info(Color::Green);
+    let colors = ColoredLevelConfig::new().info(Color::Green);
     fern::Dispatch::new()
         .format(move |out, message, record| {
             out.finish(format_args!(
@@ -63,205 +45,39 @@ async fn main() -> anyhow::Result<()> {
     let root = Path::new(&args.root).canonicalize()?;
     ensure!(root.is_dir(), "`{}` is not a directory", root.display());
 
+    let mut indexer = Index::new(&root, args.clutter.into_iter().collect(), &args.ra_bin).await?;
 
-    let ((mainloop, mut server), indexed_rx) = indexing::run_server();
-
-    let child = async_process::Command::new(&args.ra_bin)
-        .current_dir(&root)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(stderr())
-        .kill_on_drop(true)
-        .spawn()
-        .expect("Failed run rust-analyzer");
-    let stdout = child.stdout.unwrap();
-    let stdin = child.stdin.unwrap();
-
-    let mainloop_fut = tokio::spawn(async move {
-        mainloop.run_buffered(stdout, stdin).await.unwrap();
-    });
-
-    // Initialize.
-    let init_ret = server
-        .initialize(InitializeParams {
-            workspace_folders: Some(vec![WorkspaceFolder {
-                uri: Url::from_file_path(&root).unwrap(),
-                name: "root".into(),
-            }]),
-            capabilities: ClientCapabilities {
-                window: Some(WindowClientCapabilities {
-                    work_done_progress: Some(true),
-                    ..WindowClientCapabilities::default()
-                }),
-                ..ClientCapabilities::default()
-            },
-            trace: Some(TraceValue::Verbose),
-            work_done_progress_params: WorkDoneProgressParams {
-                work_done_token: Some(NumberOrString::String("GGGGG".into())),
-            },
-            initialization_options: Some(
-                serde_json::from_str(
-                    r#"{
-"files": {"excludeDirs": [".direnv", ".devenv"]},
-"workspace": {"symbol": {"search": {"limit": 10000, "kind": "all_symbols", "scope": "workspace"}}},
-"cargo": {"targetDir": "target/terrier"}
-}"#,
-                )
-                .unwrap(),
-            ),
-            ..InitializeParams::default()
-        })
-        .await
-        .unwrap();
-    info!("Initialized: {init_ret:?}");
-    server.initialized(InitializedParams {}).unwrap();
-
-    // Wait until indexed.
-    warn!("Waiting for indexing...");
-    indexed_rx.await.unwrap();
-    warn!("Indexing done.");
-
+    // Find all functions in project
     let mut spinner = Spinner::new(
         spinners::Dots,
         "Indexing functions...",
         spinoff::Color::Blue,
     );
 
-    let mut functions = HashSet::new();
-    warn!("Querying for symbols...");
-    let ret = server
-        .symbol(WorkspaceSymbolParams {
-            partial_result_params: PartialResultParams {
-                partial_result_token: None,
-            },
-            work_done_progress_params: WorkDoneProgressParams {
-                work_done_token: None,
-            },
-            query: "".into(),
-        })
-        .await
-        .context("fetching symbols")?;
-
-    if let Some(symbols) = ret {
-        match symbols {
-            WorkspaceSymbolResponse::Flat(symbols) => {
-                for ref s @ SymbolInformation {
-                    ref name,
-                    ref kind,
-                    ref container_name,
-                    ..
-                } in symbols
-                {
-                    if matches!(*kind, SymbolKind::FUNCTION) && !args.clutter.contains(name) {
-                        functions.insert(Function(s.clone()));
-                    }
-                    info!(
-                        "{:?} {}::{}",
-                        kind,
-                        container_name.clone().unwrap_or_default(),
-                        name
-                    );
-                }
-            }
-            WorkspaceSymbolResponse::Nested(workspace_symbols) => todo!(),
-        }
-    } else {
-        info!("None.");
-    }
-
-    let mut functions = functions.into_iter().collect::<Vec<_>>();
-    info!("{} functions found", functions.len());
-    functions.sort_by(|f1, f2| {
-        f1.0.location.uri.cmp(&f2.0.location.uri).then(
-            f1.0.location
-                .range
-                .start
-                .line
-                .cmp(&f2.0.location.range.start.line),
-        )
-    });
-    let function_names = functions
+    let function_names = indexer
+        .functions
         .iter()
         .map(|f| f.pretty(&root))
         .collect::<Vec<_>>();
-
     spinner.stop_and_persist("", "Functions indexed");
 
-    while let Some(selection) = FuzzySelect::new()
+    while let Some(f_id) = FuzzySelect::new()
         .items(&function_names)
         .max_length(15)
         .interact_opt()?
     {
         let mut spinner = Spinner::new(spinners::Dots, "Generating...", spinoff::Color::Blue);
-        let f = &functions[selection];
-
-        let (incomings, outgoings) = {
-            if let Some(callsites) = cache.get(f) {
-                callsites
-            } else {
-                let mut incomings = Vec::new();
-                let mut outgoings = Vec::new();
-
-                if let Some(xs) = server
-                    .incoming_calls(f.into())
-                    .await
-                    .context("failed to fetch incomings")?
-                {
-                    let mut incomings_set = HashSet::new();
-                    for CallHierarchyIncomingCall {
-                        from: ff @ CallHierarchyItem { name, .. },
-                        ..
-                    } in xs.iter()
-                    {
-                        if !args.clutter.contains(&name) {
-                            incomings_set.insert(CallSite(ff.clone()));
-                        }
-                    }
-                    incomings.extend(incomings_set.into_iter());
-                }
-
-                if let Some(xs) = server
-                    .outgoing_calls(f.into())
-                    .await
-                    .context("failed to fetch outgoings")?
-                {
-                    let mut outgoings_set = HashSet::new();
-                    for CallHierarchyOutgoingCall {
-                        to: ff @ CallHierarchyItem { name, .. },
-                        ..
-                    } in xs.iter()
-                    {
-                        if !args.clutter.contains(&name) {
-                            outgoings_set.insert(CallSite(ff.clone()));
-                        }
-                    }
-                    outgoings.extend(outgoings_set.into_iter());
-                }
-
-                incomings.sort_by(|f1, f2| {
-                    f1.0.uri
-                        .cmp(&f2.0.uri)
-                        .then(f1.0.range.start.line.cmp(&f2.0.range.start.line))
-                });
-                outgoings.sort_by(|f1, f2| {
-                    f1.0.uri
-                        .cmp(&f2.0.uri)
-                        .then(f1.0.range.start.line.cmp(&f2.0.range.start.line))
-                });
-
-                cache.insert(f.to_owned(), (incomings, outgoings));
-                cache.get(f).unwrap()
-            }
-        };
+        let (incomings, outgoings) = indexer.context(f_id).await?;
         spinner.clear();
 
         let header = [
             "".to_string(),
             "".to_string(),
-            f.pretty(&root),
+            indexer.functions[f_id].pretty(&root),
             "".to_string(),
             "".to_string(),
         ];
+
         let content = std::iter::once(header)
             .chain(
                 incomings
@@ -292,19 +108,11 @@ async fn main() -> anyhow::Result<()> {
                     }),
             );
 
-
         let table = IterTable::new(content);
         let o = table.to_string();
 
         println!("{o}");
     }
 
-    // Shutdown.
-    server.shutdown(()).await.unwrap();
-    server.exit(()).unwrap();
-
-    server.emit(Stop).unwrap();
-    mainloop_fut.await.unwrap();
-
-    Ok(())
+    indexer.shutdown().await.context("shutting down indexer")
 }
